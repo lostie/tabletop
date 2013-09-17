badee1155b47a91f8984e800a91a25912fd0bac5
Sergio Marques <sergio.rtmarques@gmail.com>
2013-09-17 23:28:55 +0100

    Added extra information about the message being published

--- app/controllers/lobbies_controller.rb
+++ app/controllers/lobbies_controller.rb
@@ -8,14 +8,16 @@ class LobbiesController < ApplicationController
         # Needs its own redis connection to pub
         # and sub at the same time
         Redis.new.subscribe "lobby" do |on|
-          on.message do |channel, message|
+          on.message do |channel, json|
+            data = JSON.parse(json)
+            message = "[#{data['time']}] #{data['user']} said: #{data['message']}"
             tubesock.send_data message
           end
         end
       end
 
       tubesock.onmessage do |msg|
-        Redis.new.publish "lobby", msg
+        Redis.new.publish "lobby", { user: 'me', time: Time.now, message: msg }.to_json
       end
 
       tubesock.onclose do
